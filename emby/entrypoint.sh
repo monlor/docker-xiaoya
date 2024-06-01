6a667b51ad43f18de2e691d2ca4b811fc1da4351
monlor <me@monlor.com>
2024-06-01 11:14:20 +0800

    fix: 🐛 恢复前置检查

--- emby/entrypoint.sh
+++ emby/entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-# echo "等待alist启动完成..."
-# while ! wget -q -T 1 -O /dev/null "${ALIST_ADDR:=http://alist:5678}" &> /dev/null; do
-#     sleep 2
-# done
+echo "等待alist启动完成..."
+while ! wget -q -T 1 -O /dev/null "${ALIST_ADDR:=http://alist:5678}" &> /dev/null; do
+    sleep 2
+done
 
-# echo "等待元数据下载完成..."
-# while test ! -f /config/emby_meta_finished; do
-#     sleep 2
-# done
+echo "等待元数据下载完成..."
+while test ! -f /config/emby_meta_finished; do
+    sleep 2
+done
 
 cat > /etc/nsswitch.conf <<-EOF
 hosts:  files dns
