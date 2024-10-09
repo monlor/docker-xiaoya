a2145c5f5215a3914662680efe70994b42a12882
monlor <me@monlor.com>
2024-10-09 22:02:21 +0800

    fix: 去掉gitlab地址
    #262

--- alist/service.sh
+++ alist/service.sh
@@ -3,7 +3,6 @@
 DATA_DIR="/www/data"
 
 base_urls=(
-    "https://gitlab.com/xiaoyaliu/data/-/raw/main"
     "https://raw.githubusercontent.com/xiaoyaliu00/data/main"
     "https://cdn.wygg.shop/https://raw.githubusercontent.com/xiaoyaliu00/data/main"
     "https://fastly.jsdelivr.net/gh/xiaoyaliu00/data@latest"
